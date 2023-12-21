d461d72b575bfd48676c15ee1c8a16d414c5714a
DVadim001 <dvadim001@gmail.com>
2023-12-21 09:21:12 +0500

    first3

---
+++ dz6/main.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+User1 = Person(name='Pavel', age=25)
+User2 = Person(name='Misha',age=30)
+
+print(User1.name)
+print(User1.age)
+print(User2.name)
+print(User2.age)
