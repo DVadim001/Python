71add62f480bee427bc7101f91dfcc201622b5fa
DVadim001 <dvadim001@gmail.com>
2024-01-04 19:16:37 +0500

    chenged_DZ8-dz15_TG_bots

--- DZ8_dz15.1/main.py
+++ DZ8_dz15.1/main.py
@@ -4,4 +4,15 @@ import  telebot
 
 bot = telebot.TeleBot("6801993152:AAFKksnzFYxVMsDZrzCcpEKvgGrOFLq3OQE")
 
-@bot.message_handler(commands=["sart"])
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id,"Здравствуйте.")
+
+@bot.message_handler(content_types=["text"])
+def text_message(message):
+    user_id = message.from_user.id
+
+
+
+bot.polling(non_stop=True)
--- DZ8_dz15.3/main.py
+++ DZ8_dz15.3/main.py
@@ -3,3 +3,12 @@
 import telebot
 
 bot = telebot.TeleBot("6643297345:AAH2Dprar4RgV7PQ4rYd0skrGPdl7cj01m0")
+
+
+
+
+
+
+
+
+bot.polling(non_stop=True)
