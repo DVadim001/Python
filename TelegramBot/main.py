a791964fcde68a1bfa7a647a100661caaacbfd51
DVadim001 <dvadim001@gmail.com>
2024-01-11 16:17:36 +0500

    bot converter

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -38,7 +38,7 @@ def wiki(message):
     user_id = message.from_user.id
     bot.send_message(user_id, "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0", reply_markup=ReplyKeyboardRemove())
     bot.send_message(user_id, "Готово, что ещё?", reply_markup=kb)
-    bot.register_next_step_handler(message,text_message)
+    bot.register_next_step_handler(message, text_message)
 def tran(message):
     user_id = message.from_user.id
     bot.send_message(user_id, "Пройдите на сайт переводчика!", reply_markup=ReplyKeyboardRemove())
