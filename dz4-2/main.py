209c26954c94e44335ea9ddd28c50098dacb509e
DVadim001 <dvadim001@gmail.com>
2024-01-18 09:21:00 +0500

    Html

--- dz4-2/main.py
+++ dz4-2/main.py
@@ -17,7 +17,7 @@ while True:
             if product_count > all_products['Весь склад'][product_to_buy]:
                 print('На складе нет такого количества')
             else:
-                korzina.append(product_to_buy, product_count) # добавить значания как в словарь, по синтаксису словаря
+                korzina.append({product_to_buy: product_count})
                 all_products['Весь склад'][product_to_buy] -= product_count
                 print('Продукт добавлен в корзину!')
         else:
@@ -29,9 +29,8 @@ while True:
 
     elif action.lower() == 'удалить':
         product_to_delete = input('Введите товар для удаления: ')
-        product_count_delete = int(input('Введите количество для удаления: ')) # нужно ли это? Он же удаляет пару ключ-значение.
-        if product_to_delete in korzina[].keys(): # обратиться к лист слайсигн и обращаться к кажд знач как к словарю.....Понимаю, что сюда нужно передать ключ, но как я его могу знать, а переменную product_to_delete он не принимает
+        product_count_delete = int(input('Введите количество для удаления: '))
+        if product_to_delete in korzina[0].keys():
             korzina.pop(product_to_delete) # исп цикл for
     else:
         print('Неизвестная операция')
-#
