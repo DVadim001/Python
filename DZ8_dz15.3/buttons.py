1203887ea9009ce5e7a7c3ed9371012fd1741df5
DVadim001 <dvadim001@gmail.com>
2024-01-20 19:27:14 +0500

    html and css

--- DZ8_dz15.3/buttons.py
+++ DZ8_dz15.3/buttons.py
@@ -1,5 +1,6 @@
 from telebot import types
 
+
 def num_bt():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     num = types.KeyboardButton("Отправить номер", request_contact=True)
@@ -11,4 +12,15 @@ def loc_bt():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     loc = types.KeyboardButton("Отправить локацию", request_location=True)
     kb.add(loc)
+    return kb
+
+
+def lang():
+    # Создаём пространство для кнопок
+    kb = types.InlineKeyboardMarkup(row_width=2)
+    # Создаём сами кнопки
+    rus = types.InlineKeyboardButton(text="Русский", callback_data="rus")
+    eng = types.InlineKeyboardButton(text="English", callback_data="eng")
+    # Добавляем кнопки в пространство
+    kb.add(rus, eng)
     return kb
--- DZ8_dz15.3/main.py
+++ DZ8_dz15.3/main.py
@@ -1,12 +1,31 @@
 # ТГ бот регистрации.
 import telebot
-import buttons as bt
 import database as db
+import buttons as bt
 from geopy import Nominatim
+
 
 bot = telebot.TeleBot("6643297345:AAH2Dprar4RgV7PQ4rYd0skrGPdl7cj01m0")
 geo = Nominatim(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                            "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+
+selected_lang = {}
+texts = {"welcome_message": {"rus": "Здравствуйте.", "eng": "Welcome."},
+         "reg_begin": {"rus": "Начало регистрации. Введите имя.", "eng": "Registration start. Enter your name."},
+         "name_res": {"rus":"Имя получено, отправьте номер.", "eng": "Name received, send the number."},
+         "loc_res": {"rus":"Номер получен, отправьте локацию.", "eng": "Number received, send the location."},
+         "send_but": {"rus":"Отправьте по кнопке.", "eng": "Send using the button."},
+         "reg_suc": {"rus":"Регистрация успешна.", "eng": "Registration successful."},
+         "lang_choose": {"rus":"Добро пожаловать. Выберите удобный для вас язык", "eng": "Welcome. Choose your preferred language"},
+         "set_lang": {"rus":"Установлен язык: Русский.", "eng": "Your language has beet set to: English"},
+         # "": {"rus":"", "eng": ""},
+         }
+
+
+def lang_choice(user_id, keyword):
+    user_lang = selected_lang.get(user_id, "rus")
+    return texts[keyword][user_lang]
+
 
 
 @bot.message_handler(commands=["start"])
@@ -14,9 +33,10 @@ def start(message):
     user_id = message.from_user.id
     check = db.check_in_base(user_id)
     if check:
-        bot.send_message(user_id, f"Здравствуйте, '{message.from_user.first_name}'.")
+        bot.send_message(user_id, f"Здравствуйте, '{message.from_user.first_name}', выберите удобный для вас язык.", reply_markup=bt.lang())
     else:
-        bot.send_message(user_id,f"Добро пожаловать, Начало регистрации. Введите имя.")
+        bot.send_message(user_id,f"Добро пожаловать. Выберите удобный для вас язык",reply_markup=bt.lang())
+        bot.send_message(user_id,f"Начало регистрации. Введите имя.")
         bot.register_next_step_handler(message, get_name)
 
 
@@ -47,6 +67,18 @@ def get_loc(message, name, num):
     else:
         bot.send_message(user_id, "Отправьте по кнопке.", reply_markup=bt.loc_bt())
         bot.register_next_step_handler(message, get_loc, name, num)
+
+
+@bot.callback_query_handler(lambda call: call.data in ['rus', 'eng'])
+def choose_lang(call):
+    chat_id = call.message.chat.id
+    user_id = call.from_user.id
+    selected_lang[user_id] = call.data
+    if call.data == "rus":
+        bot.send_message(chat_id, "Установлен язык: Русский",reply_markup=telebot.types.ReplyKeyboardRemove())
+    elif call.data == "eng":
+        bot.send_message(chat_id, "Your language has beet set to: English", reply_markup=telebot.types.ReplyKeyboardRemove())
+
 
 
 bot.polling(non_stop=True)
